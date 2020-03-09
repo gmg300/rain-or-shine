d9f95e6a2ad782bc667f27be98e0e0c96231cb53
Garrett <garrettgassensmith@gmail.com>
2020-03-09 18:34:12 -0500

    Forecast data has been retrieved from the api and displayed as cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
+  var searchHistory = {};
+
   $("#search-btn").on("click", getWeather);
+
   function getWeather() {
     $("#weather-view").empty();
-    // Current Weather
     var city = $("#search-input")
       .val()
       .trim();
@@ -37,21 +39,55 @@ $(document).ready(function() {
           lon,
         method: "GET"
       }).then(function(res) {
-          var uvIndex = res.value;
-          var background = "bg-secondary";
-          if(uvIndex <= 2.99) {
-              background = "bg-success";
-          } else if(uvIndex >= 3 && uvIndex <= 7.99) {
-              background = "bg-warning";
-          } else if(uvIndex >= 8) {
-              background = "bg-danger";
-          }
-          $("#weather-view").append(`<p>UV Index: <span class="rounded p-1 ${background}">${uvIndex}</span></p>`);
+        var uvIndex = res.value;
+        var background = "bg-secondary";
+        if (uvIndex <= 2.99) {
+          background = "bg-success";
+        } else if (uvIndex >= 3 && uvIndex <= 7.99) {
+          background = "bg-warning";
+        } else if (uvIndex >= 8) {
+          background = "bg-danger";
+        }
+        $("#weather-view").append(
+          `<p>UV Index: <span class="rounded p-1 ${background}">${uvIndex}</span></p>`
+        );
       });
     });
+    getForecast(city);
   }
 
-  var searchHistory = {};
+  function getForecast(city) {
+    $(".card-group").empty();
+    var queryURL =
+      "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      city +
+      "&appid=89361e8467bec0f20018786eac13a299";
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(res) {
+      console.log(res);
+      var day = 1;
+      for (i = 6; i < 39; i += 8) {
+        console.log(res.list[i]);
+        var date = moment()
+          .add(day, "days")
+          .format("l");
+        var temp = Math.floor((res.list[i].main.temp - 273.15) * 1.8 + 32); // Kelvin to Fahrenheit
+        var humidity = res.list[i].main.humidity;
+        var block = `<div class="card m-2 bg-primary rounded text-light">
+                        <div class="card-header">${date}</div>
+                        <div class="card-body">
+                            <i id="day1-icon" class="mb-1 fas fa-3x fa-sun"></i>
+                            <p>Temperature: ${temp}&#176;F</p>
+                            <p>Humidity: ${humidity}%</p>
+                        </div>
+                    </div>`;
+        $(".card-group").append(block);
+        day++;
+      }
+    });
+  }
 
   function getSearchHistory() {}
 
@@ -61,11 +97,5 @@ $(document).ready(function() {
 
   function renderWeather() {}
 
-  function getForecast() {}
-
   function renderForecast() {}
-
-  var forecastDates = moment()
-    .add(1, "days")
-    .format("l");
 });
