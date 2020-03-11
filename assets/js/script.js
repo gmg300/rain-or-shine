65b53c71cbba6ba28d149de35ca0037ed2f2ace7
Garrett <garrettgassensmith@gmail.com>
2020-03-11 12:40:17 -0500

    app now uses dynamically generated weather icons to display weather conditions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,36 +2,19 @@ $(document).ready(function() {
   var searchHistory = {};
 
   var weatherIcons = {
-    day: {
-      clearSky: "http://openweathermap.org/img/wn/01d@2x.png",
-      fewClouds: "http://openweathermap.org/img/wn/02d@2x.png",
-      scatteredClouds: "http://openweathermap.org/img/wn/03d@2x.png",
-      brokenClouds: "http://openweathermap.org/img/wn/04d@2x.png",
-      showerRain: "http://openweathermap.org/img/wn/09d@2x.png",
-      rain: "http://openweathermap.org/img/wn/10d@2x.png",
-      thunderstorm: "http://openweathermap.org/img/wn/11d@2x.png",
-      snow: "http://openweathermap.org/img/wn/13d@2x.png",
-      mist: "http://openweathermap.org/img/wn/50d@2x.png"
-    },
-    night: {
-      clearSky: "http://openweathermap.org/img/wn/01n@2x.png",
-      fewClouds: "http://openweathermap.org/img/wn/02n@2x.png",
-      scatteredClouds: "http://openweathermap.org/img/wn/03n@2x.png",
-      brokenClouds: "http://openweathermap.org/img/wn/04n@2x.png",
-      showerRain: "http://openweathermap.org/img/wn/09n@2x.png",
-      rain: "http://openweathermap.org/img/wn/10n@2x.png",
-      thunderstorm: "http://openweathermap.org/img/wn/11n@2x.png",
-      snow: "http://openweathermap.org/img/wn/13n@2x.png",
-      mist: "http://openweathermap.org/img/wn/50n@2x.png"
-    }
+    clear: "http://openweathermap.org/img/wn/01d@2x.png",
+    clouds: "http://openweathermap.org/img/wn/02d@2x.png",
+    drizzle: "http://openweathermap.org/img/wn/09d@2x.png",
+    rain: "http://openweathermap.org/img/wn/10d@2x.png",
+    thunderstorm: "http://openweathermap.org/img/wn/11d@2x.png",
+    snow: "http://openweathermap.org/img/wn/13d@2x.png",
+    mist: "http://openweathermap.org/img/wn/50d@2x.png"
   };
 
   $("#search-btn").on("click", getWeather);
 
-  function getWeatherIcon(conditions, currentHour) {
-    weatherIcons;
-    switch(conditions)
-
+  function getWeatherIcon(con) {
+    return icon;
   }
 
   function getWeather() {
@@ -39,7 +22,7 @@ $(document).ready(function() {
     var city = $("#search-input")
       .val()
       .trim();
-    console.log(city);
+    // console.log(city);
     var queryURL =
       "https://api.openweathermap.org/data/2.5/weather?q=" +
       city +
@@ -55,12 +38,26 @@ $(document).ready(function() {
       var temp = Math.floor((res.main.temp - 273.15) * 1.8 + 32); // Kelvin to Fahrenheit
       var humidity = res.main.humidity;
       var wind = Math.floor(res.wind.speed * 2.237); // m/s to MPH
-      var currentHour = moment().hour();
-      var conditions = res.weather.main;
-
-      console.log(res);
+      var con = res.weather[0].main;
+      var icon = "";
+      if (con == "Clear") {
+        icon = weatherIcons.clear;
+      } else if (con == "Clouds") {
+        icon = weatherIcons.clouds;
+      } else if (con == "Drizzle") {
+        icon = weatherIcons.drizzle;
+      } else if (con == "Rain") {
+        icon = weatherIcons.rain;
+      } else if (con == "Thunderstorm") {
+        icon = weatherIcons.thunderstorm;
+      } else if (con == "Snow") {
+        icon = weatherIcons.snow;
+      } else {
+        icon = weatherIcons.mist;
+      }
+      // console.log(res);
       $("#weather-view").append(
-        `<h2 id="current-city" class="pb-3">${currentCity} (${cityDate})<img src="" /></h2>`
+        `<h2 id="current-city" class="pb-3">${currentCity} (${cityDate})<img src="${icon}" /></h2>`
       );
       $("#weather-view").append(`<p>Temperature: ${temp} &#176;F</p>`);
       $("#weather-view").append(`<p>Humidity: ${humidity}%</p>`);
@@ -101,7 +98,7 @@ $(document).ready(function() {
       url: queryURL,
       method: "GET"
     }).then(function(res) {
-      console.log(res);
+      // console.log(res);
       $("#forecast-title").append("<h2>5-Day Forecast:</h2>");
       var day = 1;
       for (i = 6; i < 39; i += 8) {
@@ -111,11 +108,28 @@ $(document).ready(function() {
           .format("l");
         var temp = Math.floor((res.list[i].main.temp - 273.15) * 1.8 + 32); // Kelvin to Fahrenheit
         var humidity = res.list[i].main.humidity;
+        var con = res.list[i].weather[0].main;
+        var icon = "";
+        if (con == "Clear") {
+          icon = weatherIcons.clear;
+        } else if (con == "Clouds") {
+          icon = weatherIcons.clouds;
+        } else if (con == "Drizzle") {
+          icon = weatherIcons.drizzle;
+        } else if (con == "Rain") {
+          icon = weatherIcons.rain;
+        } else if (con == "Thunderstorm") {
+          icon = weatherIcons.thunderstorm;
+        } else if (con == "Snow") {
+          icon = weatherIcons.snow;
+        } else {
+          icon = weatherIcons.mist;
+        }
         var block = `<div class="col-sm col-md-6 col-lg-4">
                     <div class="card m-2 bg-primary rounded text-light">
                         <div class="card-header">${date}</div>
                         <div class="card-body">
-                            <i id="day1-icon" class="mb-1 fas fa-3x fa-sun"></i>
+                            <img src="${icon}" />
                             <p>Temperature: ${temp}&#176;F</p>
                             <p>Humidity: ${humidity}%</p>
                         </div>
